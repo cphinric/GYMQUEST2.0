4f37ba4ec7b049f12725007c81fb3811bcacc29c
8ightBall <parkerhinrichs914@gmail.com>
2024-08-13 14:53:11 -0500

    Initial

---
+++ app/src/main/java/com/example/gymquest20/ui/nutrition/NutritionViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.gymquest20.ui.nutrition
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NutritionViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
